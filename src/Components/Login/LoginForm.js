704e5c63865c9847f907a9f1de1bfa33014782d7
carlosGodoi <carlos-godoi@hotmail.com>
2022-04-01 18:24:29 -0300

    Ajustada as navegações entre a home e login.

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,32 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { UserContext } from '../../UserContext';
 import Button from '../Forms/Button';
 import Input from '../Forms/Input';
+import useForm from '../Hooks/useForm';
 
 export const LoginForm = () => {
-  function handleSubmit(event) {
+  const username = useForm();
+  const password = useForm();
+  const { userLogin, error, loading } = React.useContext(UserContext);
+
+  async function handleSubmit(event) {
     event.preventDefault();
-    fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(),
-    })
-      .then((response) => {
-        console.log(response);
-        return response.json();
-      })
-      .then((json) => console.log(json));
+
+    if (username.validate() && password.validate()) {
+      userLogin(username.value, password.value);
+    }
   }
 
   return (
     <section>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <Input label="Usuário" type="text" name="username" />
-        <Input label="Senha" type="password" name="password" />
-        <Button>Entrar</Button>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        {loading ? (
+          <Button disabled>Carregando...</Button>
+        ) : (
+          <Button>Entrar</Button>
+        )}
+        {error && <p>{error}</p>}
       </form>
       <Link to="/login/criar">Cadastro</Link>
     </section>
